d7539ce3617f96b27c66c3c42c6e382df21e4081
leloucj <gothicj@163.com>
2017-05-18 21:45:51 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(a) {
   // Write your cade here
+  var num =a.split("");
+  var sum=0;
+  var length=num.length();
+  for(var i=0;i<length;i++){
+  sum+=parseInt(num[i])
+  }
+  return sum
 };
